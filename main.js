06c79c2eb10928b2101f9283ee4fa92b4e0760e8
goathead1224 <152832935+goathead1224@users.noreply.github.com>
2024-06-05 22:39:38 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ $(window).scroll(function () {
 
 $('#link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
 	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-	var pos = $(elmHash).offset().top - 70;//idの上部の距離からHeaderの高さを引いた値を取得
+	var pos = $(elmHash).offset().top - 99;//idの上部の距離からHeaderの高さを引いた値を取得
 	$('body,html').animate({ scrollTop: pos }, 1000); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
 	return false;
 });
